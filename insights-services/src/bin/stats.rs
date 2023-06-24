a48263a2a74310c0ac26d2b00b7895ea14a569d2
Andrew Schineller <schineaj23@pwcs-edu.org>
2023-06-23 21:13:51 -0400

    Finally fix the timestamp problem, needed UTC

--- insights-services/src/bin/stats.rs
+++ insights-services/src/bin/stats.rs
@@ -1,10 +1,13 @@
-use std::{collections::HashMap, error::Error, fs::File, io::BufReader};
+use std::{collections::HashMap, error::Error, fs::File, io::BufReader, sync::Arc};
 
+use chrono::{DateTime, NaiveDateTime, Utc};
 use dotenv::dotenv;
 use insights::collect;
 use regex::Regex;
+use sqlx::Postgres;
 
-#[derive(Debug)]
+#[allow(dead_code)]
+#[derive(Debug, sqlx::FromRow)]
 struct LogRow {
     log_id: i32,
     unix_timestamp: i32,
@@ -14,13 +17,12 @@ struct LogRow {
 #[allow(dead_code)]
 #[derive(Debug, sqlx::FromRow)]
 struct DemoRow {
-    id: i32,
     name: String,
+    url: String,
     map: String,
     created_at: chrono::NaiveDateTime,
 }
 
-#[allow(dead_code)]
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
@@ -28,10 +30,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
 
-    let team_map: HashMap<String, collect::Team> = serde_json::from_reader(reader)?;
+    let _team_map: HashMap<String, collect::Team> = serde_json::from_reader(reader)?;
 
-    let log_pool = sqlx::PgPool::connect(&std::env::var("DATABASE_URL")?).await?;
-    let demo_pool = sqlx::PgPool::connect(&std::env::var("DEMO_DATABASE_URL")?).await?;
+    let demo_pool: sqlx::Pool<Postgres> =
+        sqlx::PgPool::connect(&std::env::var("DATABASE_URL")?).await?;
+    let log_pool = sqlx::PgPool::connect(&std::env::var("LOG_DATABASE_URL")?).await?;
 
     println!("connected to db");
 
@@ -47,86 +50,64 @@ async fn main() -> Result<(), Box<dyn Error>> {
         "granary",
     ];
 
-    let all_logs = sqlx::query_as!(
-        LogRow,
-        "select log_id, unix_timestamp, map from log order by log_id asc"
+    let all_logs: Arc<[LogRow]> = sqlx::query_as::<Postgres, LogRow>(
+        "select log_id, unix_timestamp, map from log order by log_id asc",
     )
     .fetch_all(&log_pool)
-    .await?;
+    .await?
+    .into();
 
-    println!("Logs: {}", all_logs.len());
+    println!("Collected Logs");
+
+    let mut demos: Vec<DemoRow> = Vec::new();
     let filter = Regex::new(r"_([a-z]+[^_])_?")?;
-    for log in all_logs {
-        let lc = log.map.to_lowercase();
-        let c = match filter.captures(lc.as_str()) {
+
+    for log in all_logs.iter() {
+        let captures = match filter.captures(&log.map) {
             Some(m) => m,
             None => {
                 println!("Failed on {}", log.map);
                 continue;
             }
         };
-        let pure_map = c.get(1).unwrap().as_str();
-        let filter_string = format!("%{}%", pure_map);
+        let map_unfiltered = captures.get(1).unwrap().as_str();
 
-        // select id, name, map, created_at, server from demos where (demos.created_at between to_timestamp(1684204291 + (4*3600) - 100) and to_timestamp(1684204291 + (4*3600) + 100)) and map like '%sunshine%';
+        if !maps.contains(&map_unfiltered) {
+            println!(
+                "Log({}): Map is not in map pool! Found {}",
+                log.log_id, map_unfiltered
+            );
+            continue;
+        }
 
-        let row = match sqlx::query_as::<_, DemoRow>(
-            "SELECT id, name, map, created_at FROM demos where
-            (demos.created_at between to_timestamp($1::integer + (4*3600) - 100) AND to_timestamp($1 + (4*3600) + 100))
-            AND map LIKE $2::text",
-        )
-        .bind(&log.unix_timestamp)
-        .bind(&filter_string)
-        .fetch_one(&demo_pool)
-        .await
-        {
-            Ok(row) => row,
-            Err(err) => {
-                println!(
-                    "no demo for {}, time: {}, error: {}",
-                    log.log_id,
-                    log.unix_timestamp,
-                    err.to_string()
-                );
-                continue;
+        let map = format!("%{}%", captures.get(1).unwrap().as_str());
+
+        let lower_bound = DateTime::<Utc>::from_utc(
+            NaiveDateTime::from_timestamp_opt((log.unix_timestamp as i64) - 100, 0).unwrap(),
+            Utc,
+        );
+
+        let upper_bound = DateTime::<Utc>::from_utc(
+            NaiveDateTime::from_timestamp_opt((log.unix_timestamp as i64) + 100, 0).unwrap(),
+            Utc,
+        );
+
+        let query = sqlx::query_as::<Postgres, DemoRow>("select name, url, map, created_at from demos where demos.created_at between $1 and $2 and demos.map like $3")
+        .bind(&lower_bound)
+        .bind(&upper_bound)
+        .bind(&map)
+        .fetch_one(&demo_pool).await;
+        match query {
+            Ok(demo) => {
+                println!("Log({}): {:?}", log.log_id, demo);
+                demos.push(demo);
+            }
+            Err(error) => {
+                println!("Log({}): Error {}", log.log_id, error);
             }
         };
-
-        println!("Matched Demo: {:?} for Log {}", row, log.log_id);
     }
-
-    // for (name, team) in team_map.iter() {
-    //     println!("{} map pool", name);
-    //     for map in maps {
-    //         let search = format!("%{}%", map);
-    //         let played = sqlx::query!(
-    //             "select count(*) from log where (red_team_id = $1 or blu_team_id = $1) and map like $2",
-    //             team.id,
-    //             search
-    //         )
-    //         .fetch_one(&pool)
-    //         .await?.count.unwrap();
-
-    //         let wins = sqlx::query!(
-    //             "select count(*) from log where (red_team_id = $1 and red_team_score > blu_team_score or blu_team_id = $1 and blu_team_score > red_team_score) and map like $2",
-    //             team.id,
-    //             search
-    //         )
-    //         .fetch_one(&pool)
-    //         .await?
-    //         .count
-    //         .unwrap();
-
-    //         println!(
-    //             "{:15} Cnt: {}\tWin: {}\tPct: {:.2}",
-    //             map,
-    //             played,
-    //             wins,
-    //             (wins as f64) / (played as f64)
-    //         );
-    //     }
-    //     println!("\n");
-    // }
+    println!("Demos found: {}", demos.len());
 
     Ok(())
 }
