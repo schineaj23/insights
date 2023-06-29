9692e48b320b34e9c311795e43130b872f3a139a
Andrew Schineller <therealcatberg@outlook.com>
2023-06-29 11:22:56 -0400

    work on adding ticks, NetTick not working for some reason

--- insights-services/analyzer/src/main.rs
+++ insights-services/analyzer/src/main.rs
@@ -4,14 +4,15 @@ use tf_demo_parser::{
     demo::{
         gamevent::GameEvent,
         message::Message,
-        parser::{gamestateanalyser::UserId, handler::BorrowMessageHandler, MessageHandler},
+        parser::{handler::BorrowMessageHandler, MessageHandler},
     },
     Demo, DemoParser, MatchState, MessageType, ParserState,
 };
 
 // What I am trying to do right now: Sac Efficiency Calculator
 // What is a sac? When soldier bombs into the other team (usually for the medic or demo)
-// When does this happen? On even uber situations/stalemates (x amount of time since last cap, most players are alive), Disadvantaged situations
+// When does this happen? On even uber situations/stalemates (x amount of time since last cap, most players are alive)
+// , Disadvantaged situations
 
 // Ways to identify stalemates:
 // I guess first identify stalemates so write the analyzer
@@ -31,17 +32,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+#[allow(dead_code)]
 #[derive(Default, Debug)]
 struct Stalemate {
     length: u32,
     end_tick: u32,
 }
 
+#[allow(dead_code)]
 #[derive(Default, Debug)]
 struct StalemateAnalyzer {
     state: MatchState,
     stalemates: Vec<Stalemate>,
     ticks_since_event: u32,
+    ticks_since_death: u32,
 }
 
 impl BorrowMessageHandler for StalemateAnalyzer {
@@ -64,7 +68,11 @@ impl MessageHandler for StalemateAnalyzer {
     fn handle_message(&mut self, message: &Message, tick: u32) {
         match message {
             Message::GameEvent(message) => self.handle_game_event(&message.event, tick),
-            Message::NetTick(_) => self.ticks_since_event += 1,
+            Message::NetTick(_) => {
+                self.ticks_since_event += 1;
+                self.ticks_since_death += 1;
+                println!("TickTickTickTickTickTick: {}", tick);
+            }
             _ => {}
         }
     }
@@ -75,6 +83,28 @@ impl StalemateAnalyzer {
         Self::default()
     }
 
+    fn report_stalemate(&mut self, event: &GameEvent, tick: u32) {
+        let treshold: u32 = 66 * 10;
+        if self.ticks_since_event < treshold && self.ticks_since_death < treshold {
+            println!(
+                "report_stalemate called but conds not met. last_death:  {} last_event: {}",
+                self.ticks_since_death, self.ticks_since_event
+            );
+            return;
+        }
+
+        let start = u32::max(self.ticks_since_death, self.ticks_since_event);
+        let len = tick - start;
+        println!(
+            "Stalemate finished. Length: {}, Event end trigger: {:?}",
+            len, event
+        );
+        self.stalemates.push(Stalemate {
+            length: len,
+            end_tick: tick,
+        });
+    }
+
     fn handle_game_event(&mut self, event: &GameEvent, tick: u32) {
         match event {
             GameEvent::TeamPlayRoundStart(e) => {
@@ -83,16 +113,18 @@ impl StalemateAnalyzer {
             }
             GameEvent::TeamPlayPointCaptured(e) => {
                 println!("Tick: {}, Capture: {:?}", tick, e);
+                self.report_stalemate(event, tick);
                 self.ticks_since_event = 0
             }
             GameEvent::PlayerChargeDeployed(e) => {
                 println!("Tick: {}, Charge: {:?}", tick, e);
+                self.report_stalemate(event, tick);
                 self.ticks_since_event = 0
             }
-            GameEvent::RocketJumpLanded(e) => {
+            GameEvent::RocketJumpLanded(_e) => {
                 // println!("Tick: {}, Jump: {:?}", tick, e);
             }
-            GameEvent::PlayerDeath(e) => {}
+            GameEvent::PlayerDeath(_) => self.ticks_since_death = 0,
             _ => {}
         }
     }
