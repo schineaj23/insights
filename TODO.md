b661c40073ac4507021af7d7945401000974aedc
Andrew Schineller <therealcatberg@outlook.com>
2023-07-09 19:38:43 -0400

    Use playerclass to check for bomb attempt

--- TODO.md
+++ TODO.md
@@ -1,9 +1,11 @@
 # TODO LIST
-- DONE: Get list of all players (INVITE DONE & advanced) 
+
+- DONE: Get list of all players (INVITE DONE & advanced)
 - DONE: Get ONLY SCRIM logs, (make sure that the core players are on the same team, around scrim times) after end of season, so data is recent
-- DONE: Ensure no duplicates 
+- DONE: Ensure no duplicates
 
 What would I need to store?
+
 - teams involved
 - who won
 - link to log
@@ -13,8 +15,8 @@ What would I need to store?
 I could just JSON this all to my own PC and process the data on my own
 I believe getting all the logs then organizing it in schema -> to db is better + more learning?
 
-
 Later
+
 - DONE: Put into DB, (find DB to use start learning it)
 - Extract insights, predicting who will win by round, etc
 - Generate power rankings
@@ -22,11 +24,16 @@ Later
 Win percentage across maps, adjusted for each opponent
 
 Advanced stats that I want to track:
+
 - Sac efficiency (times that sac killed/forced medic, or killed demoman per attempts)
   This has to be only when people
 - Stalemate breaks
 - Team fight (dry fights/mids) %
 - Uber fight %
+- Defensive power rating(parking the bus)/offensive power rating
+- Midfight to round conversion
+- Average round win length (do they win rolls or close rounds?)
+- Caps per round win
 
 - player impact rating (impact + kills + ???) similar to hltv
 
@@ -36,6 +43,7 @@ write an analyzer (similar to the collector) which will use demo parsing to get 
 ideally this parser does not run on one thread and dispatches workers to parse and add because the collector is already very slow
 
 Very Long down the road...
-- Real-time score prediction/round prediction based on current gamestate 
+
+- Real-time score prediction/round prediction based on current gamestate
   (would need to parse demos for this data)
   also this is kind of a different type of service entirely
--- insights-services/analyzer/src/main.rs
+++ insights-services/analyzer/src/main.rs
@@ -1,13 +1,17 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 use tf_demo_parser::{
     demo::{
-        data::DemoTick,
+        data::{DemoTick, UserInfo},
         gamevent::GameEvent,
-        message::Message,
-        parser::{handler::BorrowMessageHandler, MessageHandler},
+        message::{
+            packetentities::{EntityId, PacketEntity},
+            Message,
+        },
+        parser::{gamestateanalyser::UserId, handler::BorrowMessageHandler, MessageHandler},
+        sendprop::SendPropIdentifier,
     },
-    Demo, DemoParser, MatchState, MessageType, ParserState,
+    Demo, DemoParser, MessageType, ParserState,
 };
 
 // What I am trying to do right now: Sac Efficiency Calculator
@@ -24,11 +28,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let demo = Demo::new(&file);
     // let parser = DemoParser::new(demo.get_stream());
 
-    let analyzer = StalemateAnalyzer::new();
+    let analyzer = InsightsAnalyzer::new();
     let parser = DemoParser::new_with_analyser(demo.get_stream(), analyzer);
-    let (_header, stalemates) = parser.parse()?;
-    println!("Stalemates: {:?}", stalemates);
-    println!("Num: {}", stalemates.len());
+    let (_header, bomb_attempts) = parser.parse()?;
+
+    let bomb_dmg: Vec<&BombAttempt> = bomb_attempts
+        .iter()
+        .filter(|attempt| attempt.damage > 0)
+        .collect();
+    println!("Stalemates: {:?}", bomb_attempts);
+    println!("Num dmg>0: {}", bomb_dmg.len());
+    println!("Num Total: {}", bomb_attempts.len());
 
     // println!("Header: {:#?}\n\n", header);
     // println!("Rounds: {:#?}", state);
@@ -44,50 +54,156 @@ struct Stalemate {
     end_event: GameEvent,
 }
 
+// Bomb efficiency
+// Starts on RocketJumpLanded.
+// If kill/opposing medic force/ then successful within threshold
+// If die without any of these conditions (within threshold), unsucessful.
+
+// things to consider: jumps that were just for reposition/spamming
+// how do i make sure that they have jumped enough into the teams?
+
+// Jump in
+// Deal damage
+// (potentially force/get kill/etc)
+// die (or live?)
+
 #[allow(dead_code)]
 #[derive(Default, Debug)]
-struct StalemateAnalyzer {
-    state: MatchState,
+struct InsightsAnalyzer {
+    users: HashMap<UserId, tf_demo_parser::demo::parser::analyser::UserInfo>,
     stalemates: Vec<Stalemate>,
     ticks_since_event: u32,
     ticks_since_death: u32,
+
+    jumps: Vec<BombAttempt>,
+    jumpers: HashMap<u16, BombAttempt>,
+
+    class_names: Vec<tf_demo_parser::demo::packet::datatable::ServerClassName>,
+    player_data: HashMap<UserId, Player>,
 }
 
-impl BorrowMessageHandler for StalemateAnalyzer {
+#[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq)]
+struct BombAttempt {
+    user: u16,
+    start_tick: DemoTick,
+    land_tick: DemoTick,
+    damage: u16,
+    state: BombState,
+}
+
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+enum BombState {
+    InProgress,
+    TimedOut,
+    _NewJumpStarted,
+    Died,
+}
+
+#[derive(Default, Debug)]
+struct Player {
+    on_ground: bool,
+}
+
+impl BorrowMessageHandler for InsightsAnalyzer {
     fn borrow_output(&self, _state: &ParserState) -> &Self::Output {
-        &self.stalemates
+        &self.jumps
     }
 }
 
-impl MessageHandler for StalemateAnalyzer {
-    type Output = Vec<Stalemate>;
+impl MessageHandler for InsightsAnalyzer {
+    type Output = Vec<BombAttempt>;
 
     fn does_handle(message_type: tf_demo_parser::MessageType) -> bool {
-        matches!(message_type, MessageType::GameEvent | MessageType::NetTick)
+        matches!(
+            message_type,
+            MessageType::GameEvent | MessageType::NetTick | MessageType::PacketEntities
+        )
     }
 
     fn into_output(self, _state: &ParserState) -> Self::Output {
-        self.stalemates
+        self.jumps
     }
-
-    fn handle_message(&mut self, message: &Message, tick: DemoTick, _state: &ParserState) {
+    fn handle_message(&mut self, message: &Message, tick: DemoTick, state: &ParserState) {
         match message {
+            Message::PacketEntities(message) => {
+                for entity in &message.entities {
+                    self.handle_entity(entity, state);
+                }
+            }
             Message::GameEvent(message) => self.handle_game_event(&message.event, tick),
             Message::NetTick(_) => {
                 self.ticks_since_event += 1;
                 self.ticks_since_death += 1;
+
+                self.jumpers.retain(|_, attempt| {
+                    let p = self.player_data.get(&attempt.user.into()).unwrap();
+                    if u32::from(tick) - u32::from(attempt.land_tick) < 66 * 5 || !p.on_ground {
+                        true
+                    } else {
+                        attempt.state = BombState::TimedOut;
+                        println!("Removing attempt {:?}", attempt);
+                        self.jumps.push(attempt.clone());
+                        false
+                    }
+                });
+            }
+            Message::ClassInfo(e) => {
+                println!("{:?}", e)
             }
             _ => {}
         }
     }
+
+    // Taken from demostf parser analyzer example
+    // Trying to understand all these data structures lmao
+    fn handle_string_entry(
+        &mut self,
+        table: &str,
+        index: usize,
+        entry: &tf_demo_parser::demo::packet::stringtable::StringTableEntry,
+        _parser_state: &ParserState,
+    ) {
+        if table != "userinfo" {
+            return;
+        }
+
+        if let Some(info) = UserInfo::parse_from_string_table(
+            index as u16,
+            entry.text.as_ref().map(|s| s.as_ref()),
+            entry.extra_data.as_ref().map(|data| data.data.clone()),
+        )
+        .unwrap()
+        {
+            self.users
+                .entry(info.player_info.user_id)
+                .and_modify(|info| {
+                    info.entity_id = info.entity_id;
+                })
+                .or_insert_with(|| info.into());
+        }
+    }
+
+    fn handle_data_tables(
+        &mut self,
+        _tables: &[tf_demo_parser::demo::packet::datatable::ParseSendTable],
+        server_classes: &[tf_demo_parser::demo::packet::datatable::ServerClass],
+        _parser_state: &ParserState,
+    ) {
+        self.class_names = server_classes
+            .iter()
+            .map(|class| &class.name)
+            .cloned()
+            .collect();
+    }
 }
 
-impl StalemateAnalyzer {
+impl InsightsAnalyzer {
     pub fn new() -> Self {
         Self::default()
     }
 
-    fn report_stalemate(&mut self, event: &GameEvent, tick: DemoTick) {
+    fn _report_stalemate(&mut self, event: &GameEvent, tick: DemoTick) {
         let treshold: u32 = 66 * 15;
         if self.ticks_since_event < treshold && self.ticks_since_death < treshold {
             println!(
@@ -124,17 +240,133 @@ impl StalemateAnalyzer {
             }
             GameEvent::TeamPlayPointCaptured(e) => {
                 println!("Tick: {:?}, Capture: {:?}", tick, e);
-                self.report_stalemate(event, tick);
+                // self.report_stalemate(event, tick);
             }
             GameEvent::PlayerChargeDeployed(e) => {
                 println!("Tick: {:?}, Charge: {:?}", tick, e);
-                self.report_stalemate(event, tick);
+                // self.report_stalemate(event, tick);
             }
-            GameEvent::RocketJumpLanded(_) => {
-                // println!("Tick: {:?}, Jump: {:?}", tick, e);
+            GameEvent::RocketJumpLanded(e) => {
+                self.jumpers.entry(e.user_id).and_modify(|attempt| {
+                    let user = self.users.get(&e.user_id.into()).unwrap();
+                    println!(
+                        "Rocket Jump Landed: {}, TickSinceStart: {}",
+                        u32::from(tick) - u32::from(attempt.start_tick),
+                        user.name
+                    );
+                    attempt.land_tick = tick;
+                });
             }
-            GameEvent::PlayerDeath(_) => self.ticks_since_death = 0,
+            GameEvent::RocketJump(e) => {
+                let user = match self.users.get(&e.user_id.into()) {
+                    Some(user) => user,
+                    None => return,
+                };
+                println!("Rocket Jump Started for {}", user.name);
+
+                if self.jumpers.contains_key(&e.user_id) {
+                    // let mut attempt = self.jumpers.remove(&e.user_id).unwrap();
+                    // attempt.state = BombState::NewJumpStarted;
+                    // self.jumps.push(attempt);
+                } else {
+                    self.jumpers.insert(
+                        e.user_id,
+                        BombAttempt {
+                            user: e.user_id,
+                            start_tick: tick,
+                            land_tick: DemoTick::from(0),
+                            damage: 0,
+                            state: BombState::InProgress,
+                        },
+                    );
+                }
+            }
+            GameEvent::PlayerDeath(e) => {
+                self.ticks_since_death = 0;
+
+                if self.jumpers.contains_key(&e.user_id) {
+                    let mut attempt = self.jumpers.remove(&e.user_id).unwrap();
+                    attempt.state = BombState::Died;
+                    self.jumps.push(attempt);
+                    return;
+                }
+
+                if self.jumpers.contains_key(&e.attacker) {
+                    let attacker = match self.users.get(&e.attacker.into()) {
+                        Some(user) => user,
+                        None => return,
+                    };
+                    let victim = match self.users.get(&e.user_id.into()) {
+                        Some(user) => user,
+                        None => return,
+                    };
+                    println!("Jumper {} killed user {}", attacker.name, victim.name);
+                }
+            }
+            GameEvent::PlayerHurt(e) => {
+                if !self.jumpers.contains_key(&e.attacker) || self.jumpers.contains_key(&e.user_id)
+                {
+                    return;
+                }
+
+                let attacker = match self.users.get(&e.attacker.into()) {
+                    Some(user) => user,
+                    None => return,
+                };
+                let victim = match self.users.get(&e.user_id.into()) {
+                    Some(user) => user,
+                    None => return,
+                };
+                self.jumpers.get_mut(&e.attacker).unwrap().damage += e.damage_amount;
+                println!(
+                    "Jumper {} damaged {} for {}",
+                    attacker.name, victim.name, e.damage_amount
+                );
+            }
+            // e => println!("{:?}", e),
             _ => {}
+        }
+    }
+
+    // FIXME: use another data structure
+    fn get_or_insert_player_from_entity_id(&mut self, entity_id: EntityId) -> &mut Player {
+        let id = self
+            .users
+            .iter()
+            .find(|(_, info)| info.entity_id == entity_id)
+            .map(|(id, _)| id)
+            .cloned()
+            .unwrap_or_default();
+        self.player_data
+            .entry(id)
+            .or_insert_with(|| Player::default())
+    }
+
+    fn handle_entity(&mut self, entity: &PacketEntity, parser_state: &ParserState) {
+        let class_name = self
+            .class_names
+            .get(usize::from(entity.server_class))
+            .map(|server_name| server_name.as_str())
+            .unwrap_or("");
+        match class_name {
+            "CTFPlayer" => self.handle_player_entity(entity, parser_state),
+            _ => {}
+        }
+    }
+
+    fn handle_player_entity(&mut self, entity: &PacketEntity, parser_state: &ParserState) {
+        let player = self.get_or_insert_player_from_entity_id(entity.entity_index);
+        const FLAGS_PROP: SendPropIdentifier = SendPropIdentifier::new("DT_BasePlayer", "m_fFlags");
+        const FL_ONGROUND: i64 = 1 << 0;
+
+        for prop in entity.props(parser_state) {
+            match prop.identifier {
+                FLAGS_PROP => {
+                    let flags = i64::try_from(&prop.value).unwrap_or_default();
+                    player.on_ground = (flags | FL_ONGROUND) == 0;
+                }
+                _ => {}
+            }
         }
     }
 }
