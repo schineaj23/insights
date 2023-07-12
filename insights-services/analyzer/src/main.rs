a2acac14bff32a966ce24851514029dd3cd246b1
Andrew Schineller <therealcatberg@outlook.com>
2023-07-12 18:36:47 -0400

    Filter bombs by landing position

--- insights-services/analyzer/src/main.rs
+++ insights-services/analyzer/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fs, time::Instant};
 
 use tf_demo_parser::{
     demo::{
@@ -8,8 +8,13 @@ use tf_demo_parser::{
             packetentities::{EntityId, PacketEntity},
             Message,
         },
-        parser::{gamestateanalyser::UserId, handler::BorrowMessageHandler, MessageHandler},
+        parser::{
+            gamestateanalyser::{Team, UserId},
+            handler::BorrowMessageHandler,
+            MessageHandler,
+        },
         sendprop::SendPropIdentifier,
+        vector::VectorXY,
     },
     Demo, DemoParser, MessageType, ParserState,
 };
@@ -24,24 +29,62 @@ use tf_demo_parser::{
 // Base case: soldier dies when all other 11 players are alive on similar uber scenarios
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file = fs::read("process_froyo.dem")?;
+    let file = fs::read("bagel_froyo.dem")?;
     let demo = Demo::new(&file);
     // let parser = DemoParser::new(demo.get_stream());
+
+    let start = Instant::now();
 
     let analyzer = InsightsAnalyzer::new();
     let parser = DemoParser::new_with_analyser(demo.get_stream(), analyzer);
     let (_header, bomb_attempts) = parser.parse()?;
 
+    let t = start.elapsed().as_secs_f32();
+
+    println!("All attempts");
+    bomb_attempts.iter().for_each(|x| {
+        println!("{:?}", x);
+    });
+
     let bomb_dmg: Vec<&BombAttempt> = bomb_attempts
         .iter()
         .filter(|attempt| attempt.damage > 0)
         .collect();
-    println!("Stalemates: {:?}", bomb_attempts);
+
+    let mut dmg: HashMap<u16, (i32, i32, u32)> = HashMap::new();
+
+    let mut a = 0;
+    bomb_attempts.iter().for_each(|x| {
+        a += x.damage;
+        dmg.entry(x.user)
+            .and_modify(|u| {
+                u.0 += 1;
+                u.1 += i32::from(x.damage > 0);
+                u.2 += x.damage as u32;
+            })
+            .or_insert((1, 0, x.damage as u32));
+    });
+
+    for (uid, (cnt, cnt_dmg, dmg)) in dmg {
+        println!(
+            "Uid: {}, NumBombs: {}, Dmg/Bomb: {}, NonzeroDmgBombs: {}",
+            uid,
+            cnt,
+            dmg as f64 / cnt as f64,
+            cnt_dmg
+        );
+    }
+
+    println!(
+        "Average dmg/bombattempt: {}",
+        (a as f32 / bomb_attempts.len() as f32)
+    );
+
+    // println!("Stalemates: {:?}", bomb_attempts);
     println!("Num dmg>0: {}", bomb_dmg.len());
     println!("Num Total: {}", bomb_attempts.len());
+    println!("Took {} seconds to analyze demo.", t);
 
-    // println!("Header: {:#?}\n\n", header);
-    // println!("Rounds: {:#?}", state);
     Ok(())
 }
 
@@ -71,15 +114,20 @@ struct Stalemate {
 #[derive(Default, Debug)]
 struct InsightsAnalyzer {
     users: HashMap<UserId, tf_demo_parser::demo::parser::analyser::UserInfo>,
-    stalemates: Vec<Stalemate>,
-    ticks_since_event: u32,
-    ticks_since_death: u32,
 
     jumps: Vec<BombAttempt>,
     jumpers: HashMap<u16, BombAttempt>,
 
     class_names: Vec<tf_demo_parser::demo::packet::datatable::ServerClassName>,
     player_data: HashMap<UserId, Player>,
+
+    round_state: RoundState,
+}
+
+#[derive(Default, Debug)]
+struct RoundState {
+    has_match_started: bool,
+    is_dead_time: bool,
 }
 
 #[allow(dead_code)]
@@ -87,8 +135,9 @@ struct InsightsAnalyzer {
 struct BombAttempt {
     user: u16,
     start_tick: DemoTick,
-    land_tick: DemoTick,
+    land_tick: Option<DemoTick>,
     damage: u16,
+    damage_taken: u16,
     state: BombState,
 }
 
@@ -96,13 +145,15 @@ struct BombAttempt {
 enum BombState {
     InProgress,
     TimedOut,
-    _NewJumpStarted,
+    NewJumpStarted,
     Died,
 }
 
 #[derive(Default, Debug)]
 struct Player {
     on_ground: bool,
+    position: VectorXY,
+    team: Team,
 }
 
 impl BorrowMessageHandler for InsightsAnalyzer {
@@ -124,6 +175,7 @@ impl MessageHandler for InsightsAnalyzer {
     fn into_output(self, _state: &ParserState) -> Self::Output {
         self.jumps
     }
+
     fn handle_message(&mut self, message: &Message, tick: DemoTick, state: &ParserState) {
         match message {
             Message::PacketEntities(message) => {
@@ -133,19 +185,20 @@ impl MessageHandler for InsightsAnalyzer {
             }
             Message::GameEvent(message) => self.handle_game_event(&message.event, tick),
             Message::NetTick(_) => {
-                self.ticks_since_event += 1;
-                self.ticks_since_death += 1;
-
                 self.jumpers.retain(|_, attempt| {
-                    let p = self.player_data.get(&attempt.user.into()).unwrap();
-                    if u32::from(tick) - u32::from(attempt.land_tick) < 66 * 5 || !p.on_ground {
-                        true
-                    } else {
+                    // let p = self.player_data.get(&attempt.user.into()).unwrap();
+                    let cond = match attempt.land_tick {
+                        Some(land_tick) => u32::from(tick) - u32::from(land_tick) < 66 * 3,
+                        None => true,
+                    };
+
+                    if !cond {
                         attempt.state = BombState::TimedOut;
-                        println!("Removing attempt {:?}", attempt);
+                        println!("Timing out {:?}", attempt);
                         self.jumps.push(attempt.clone());
-                        false
                     }
+
+                    cond
                 });
             }
             Message::ClassInfo(e) => {
@@ -177,8 +230,8 @@ impl MessageHandler for InsightsAnalyzer {
         {
             self.users
                 .entry(info.player_info.user_id)
-                .and_modify(|info| {
-                    info.entity_id = info.entity_id;
+                .and_modify(|i| {
+                    i.entity_id = info.entity_id;
                 })
                 .or_insert_with(|| info.into());
         }
@@ -203,127 +256,105 @@ impl InsightsAnalyzer {
         Self::default()
     }
 
-    fn _report_stalemate(&mut self, event: &GameEvent, tick: DemoTick) {
-        let treshold: u32 = 66 * 15;
-        if self.ticks_since_event < treshold && self.ticks_since_death < treshold {
-            println!(
-                "report_stalemate called but conds not met. last_death:  {} last_event: {}",
-                self.ticks_since_death, self.ticks_since_event
-            );
-            return;
-        }
-
-        let len = u32::max(self.ticks_since_death, self.ticks_since_event);
-        let start = tick - len;
-
-        println!(
-            "Stalemate finished. Length: {:?}, Event end trigger: {:?}",
-            len, event
-        );
-
-        self.stalemates.push(Stalemate {
-            length: len,
-            start_tick: start,
-            end_tick: tick,
-            end_event: event.clone(),
-        });
-
-        self.ticks_since_event = 0;
+    fn should_record(&self) -> bool {
+        return self.round_state.has_match_started && !self.round_state.is_dead_time;
     }
 
     fn handle_game_event(&mut self, event: &GameEvent, tick: DemoTick) {
         match event {
             GameEvent::TeamPlayRoundStart(e) => {
+                if !self.round_state.has_match_started {
+                    self.round_state.has_match_started = true;
+                }
+                self.round_state.is_dead_time = false;
                 println!("Tick: {:?}, Start: {:?}", tick, e);
-                self.ticks_since_event = 0;
-                self.ticks_since_death = 0;
             }
+            GameEvent::TeamPlayRoundWin(_) => self.round_state.is_dead_time = true,
+            GameEvent::TeamPlayRoundStalemate(_) => self.round_state.is_dead_time = true,
             GameEvent::TeamPlayPointCaptured(e) => {
                 println!("Tick: {:?}, Capture: {:?}", tick, e);
-                // self.report_stalemate(event, tick);
             }
             GameEvent::PlayerChargeDeployed(e) => {
                 println!("Tick: {:?}, Charge: {:?}", tick, e);
-                // self.report_stalemate(event, tick);
             }
             GameEvent::RocketJumpLanded(e) => {
+                if !self.should_record() {
+                    return;
+                }
+
+                // FIXME: remove this if it is bad
+                if self.jumpers.contains_key(&e.user_id) {
+                    print!("Tick: {:?} ", tick);
+                    if !self.in_range(e.user_id) {
+                        println!(
+                            "Removing ({}) {} because not in range.",
+                            e.user_id,
+                            self.users.get(&e.user_id.into()).unwrap().name
+                        );
+                        self.jumpers.remove(&e.user_id);
+                    }
+                }
+
                 self.jumpers.entry(e.user_id).and_modify(|attempt| {
-                    let user = self.users.get(&e.user_id.into()).unwrap();
-                    println!(
-                        "Rocket Jump Landed: {}, TickSinceStart: {}",
-                        u32::from(tick) - u32::from(attempt.start_tick),
-                        user.name
-                    );
-                    attempt.land_tick = tick;
+                    attempt.land_tick = Some(tick);
                 });
             }
             GameEvent::RocketJump(e) => {
-                let user = match self.users.get(&e.user_id.into()) {
-                    Some(user) => user,
-                    None => return,
-                };
-                println!("Rocket Jump Started for {}", user.name);
+                if !self.should_record() {
+                    return;
+                }
 
                 if self.jumpers.contains_key(&e.user_id) {
-                    // let mut attempt = self.jumpers.remove(&e.user_id).unwrap();
-                    // attempt.state = BombState::NewJumpStarted;
-                    // self.jumps.push(attempt);
+                    self.jumpers.entry(e.user_id).and_modify(|attempt| {
+                        attempt.state = BombState::NewJumpStarted;
+                    });
                 } else {
                     self.jumpers.insert(
                         e.user_id,
                         BombAttempt {
                             user: e.user_id,
                             start_tick: tick,
-                            land_tick: DemoTick::from(0),
+                            land_tick: None,
                             damage: 0,
+                            damage_taken: 0,
                             state: BombState::InProgress,
                         },
                     );
                 }
             }
             GameEvent::PlayerDeath(e) => {
-                self.ticks_since_death = 0;
+                if !self.should_record() {
+                    return;
+                }
 
                 if self.jumpers.contains_key(&e.user_id) {
                     let mut attempt = self.jumpers.remove(&e.user_id).unwrap();
                     attempt.state = BombState::Died;
+                    println!("Saving {:?}", attempt);
                     self.jumps.push(attempt);
                     return;
                 }
-
-                if self.jumpers.contains_key(&e.attacker) {
-                    let attacker = match self.users.get(&e.attacker.into()) {
-                        Some(user) => user,
-                        None => return,
-                    };
-                    let victim = match self.users.get(&e.user_id.into()) {
-                        Some(user) => user,
-                        None => return,
-                    };
-                    println!("Jumper {} killed user {}", attacker.name, victim.name);
-                }
             }
             GameEvent::PlayerHurt(e) => {
-                if !self.jumpers.contains_key(&e.attacker) || self.jumpers.contains_key(&e.user_id)
+                if !self.jumpers.contains_key(&e.attacker)
+                    || !self.jumpers.contains_key(&e.user_id)
+                    || !self.should_record()
                 {
                     return;
                 }
 
-                let attacker = match self.users.get(&e.attacker.into()) {
-                    Some(user) => user,
-                    None => return,
-                };
-                let victim = match self.users.get(&e.user_id.into()) {
-                    Some(user) => user,
-                    None => return,
-                };
-                self.jumpers.get_mut(&e.attacker).unwrap().damage += e.damage_amount;
-                println!(
-                    "Jumper {} damaged {} for {}",
-                    attacker.name, victim.name, e.damage_amount
-                );
+                self.jumpers.entry(e.attacker).and_modify(|x| {
+                    if e.user_id != e.attacker {
+                        x.damage += e.damage_amount;
+                    }
+                });
+
+                self.jumpers.entry(e.user_id).and_modify(|x| {
+                    if e.attacker != e.user_id {}
+                    // TODO: FIXME: NOTE: this is including the damage taken from the blast itself
+                    x.damage_taken += e.damage_amount;
+                });
             }
-            // e => println!("{:?}", e),
             _ => {}
         }
     }
@@ -357,6 +388,12 @@ impl InsightsAnalyzer {
     fn handle_player_entity(&mut self, entity: &PacketEntity, parser_state: &ParserState) {
         let player = self.get_or_insert_player_from_entity_id(entity.entity_index);
         const FLAGS_PROP: SendPropIdentifier = SendPropIdentifier::new("DT_BasePlayer", "m_fFlags");
+        const TEAM_PROP: SendPropIdentifier =
+            SendPropIdentifier::new("DT_BaseEntity", "m_iTeamNum");
+        const LOCAL_ORIGIN: SendPropIdentifier =
+            SendPropIdentifier::new("DT_TFLocalPlayerExclusive", "m_vecOrigin");
+        const NON_LOCAL_ORIGIN: SendPropIdentifier =
+            SendPropIdentifier::new("DT_TFNonLocalPlayerExclusive", "m_vecOrigin");
         const FL_ONGROUND: i64 = 1 << 0;
 
         for prop in entity.props(parser_state) {
@@ -365,8 +402,37 @@ impl InsightsAnalyzer {
                     let flags = i64::try_from(&prop.value).unwrap_or_default();
                     player.on_ground = (flags | FL_ONGROUND) == 0;
                 }
+                LOCAL_ORIGIN | NON_LOCAL_ORIGIN => {
+                    player.position = VectorXY::try_from(&prop.value).unwrap_or_default();
+                }
+                TEAM_PROP => {
+                    player.team = Team::new(i64::try_from(&prop.value).unwrap_or_default())
+                }
                 _ => {}
             }
         }
+    }
+
+    // TODO: possibly separate waddling skips vs high bombs, since small skip/spam gets detected if < 750, but fades dont?
+
+    // Gets the distance to the closest player by iterating over every enemy and taking pythagorean distance
+    fn in_range(&self, id: u16) -> bool {
+        let player: &Player = self.player_data.get(&id.into()).unwrap();
+        let pos = player.position;
+        let mut min = f32::MAX;
+        self.player_data.iter().for_each(|(_, p)| {
+            if player.team == p.team || p.team == Team::Other || p.team == Team::Spectator {
+                return;
+            }
+            let dist =
+                f32::sqrt(f32::powi(p.position.x - pos.x, 2) + f32::powi(p.position.y - pos.y, 2));
+            min = f32::min(min, dist);
+        });
+        println!(
+            "Closest player to {} is {} far away",
+            self.users.get(&id.into()).unwrap().name,
+            min
+        );
+        min < 500f32
     }
 }
