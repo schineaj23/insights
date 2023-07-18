74d7bd2578f20289de76ef0aa589b0ff8e59db9f
Andrew Schineller <schineaj23@pwcs-edu.org>
2023-07-17 22:45:24 -0400

    Update schema to join demos to logs

--- schema.sql
+++ schema.sql
@@ -33,7 +33,6 @@ CREATE TABLE IF NOT EXISTS player_stats(
     drops INTEGER,
     headshots INTEGER,
     headshots_hit INTEGER,
-
     FOREIGN KEY (log_id) REFERENCES log (log_id)
 );
 CREATE TABLE IF NOT EXISTS team_stats(
@@ -48,7 +47,55 @@ CREATE TABLE IF NOT EXISTS team_stats(
     drops INTEGER,
     first_caps INTEGER,
     caps INTEGER,
-
     FOREIGN KEY (log_id) REFERENCES log (log_id),
     FOREIGN KEY (team_id) REFERENCES team (team_id)
 );
+CREATE EXTENSION IF NOT EXISTS postgres_fdw;
+CREATE SERVER IF NOT EXISTS insights_demos_server FOREIGN DATA WRAPPER postgres_fdw OPTIONS (
+    host '127.0.0.1',
+    dbname 'insights-dev-demos',
+    port '5432'
+);
+\
+set db_username 'usrname' \
+set db_password 'passwrd' CREATE USER MAPPING FOR cat SERVER insights_demos_server OPTIONS (user :'db_username', password :'db_password');
+IMPORT FOREIGN SCHEMA public
+LIMIT TO demos
+FROM SERVER insights_demos_server INTO public;
+drop materialized view if exists connected_demos;
+SET TIME ZONE 'UTC';
+create materialized view connected_demos as
+select demos.name,
+    demos.id,
+    demos.url,
+    demos.created_at,
+    log.log_id,
+    log.red_team_id,
+    log.blu_team_id,
+    log.red_team_score,
+    log.blu_team_score,
+    log.map
+from demos
+    right join log on (
+        demos.created_at between to_timestamp(log.unix_timestamp - 30)::timestamp without time zone and to_timestamp(log.unix_timestamp + 100)::timestamp without time zone
+        and demos.map = log.map
+        and abs(
+            (demos."scoreBlue" - log.blu_team_score) + (log.red_team_score - demos."scoreRed")
+        ) < 2
+        and exists(
+            select player_steamid64
+            from player_stats
+            where player_stats.log_id = log.log_id
+                and cast(player_steamid64 as varchar) in (
+                    select steamid
+                    from users
+                    where id in (
+                            select user_id
+                            from players
+                            where demo_id = demos.id
+                        )
+                )
+        )
+    )
+where log.map is not null
+order by(log.log_id);
