d8db015e5b3f3e2f3f6da5ac87e8c82b672dc1cb
Andrew Schineller <therealcatberg@outlook.com>
2023-08-02 22:25:54 -0400

    Remove unnecessary comment

--- insights-web/vite.config.js
+++ insights-web/vite.config.js
@@ -2,6 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	// optimizeDeps: { exclude: ['nodejs-polars-linux-x64-musl', 'nodejs-polars-linux-x64-gnu'] },
 	plugins: [sveltekit()]
 });
