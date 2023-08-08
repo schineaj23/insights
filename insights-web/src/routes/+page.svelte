4acd86eb816650c2c19998baad9384aaa8571b18
Andrew Schineller <therealcatberg@outlook.com>
2023-08-08 19:23:32 -0400

    Add tailwind for frontend, revise bomb page

--- insights-web/src/routes/+page.svelte
+++ insights-web/src/routes/+page.svelte
@@ -20,16 +20,72 @@
 	];
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Plot
-	data={plotData}
-	layout={{
-		title: 'Average Bomb Damage vs Number of Attempts',
-		xaxis: { title: 'Average Bomb Damage' },
-		yaxis: { title: 'Number of Attempts' },
-		font: { family: 'Arial' },
-		showlegend: false
-	}}
-	debounce={250}
-/>
+<div class="container max-w-xl my-10 p-10 shadow-md">
+	<div class="flex flex-col justify-center gap-4">
+		<p class="text-5xl font-bold subpixel-antialiased">Tracking Bomb Efficiency</p>
+		<p class="">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ipsa dolorem voluptatem
+			dignissimos eaque. Sunt sapiente facilis tempore, debitis doloremque quisquam aut voluptatem
+			illo itaque tempora ipsum dicta, sequi porro?
+		</p>
+
+		<div class="max-w-lg rounded-xl shadow-md border">
+			<Plot
+				data={plotData}
+				layout={{
+					title: 'Average Bomb Damage vs Number of Attempts',
+					xaxis: { title: 'Average Bomb Damage' },
+					yaxis: { title: 'Number of Attempts' },
+					font: {
+						family:
+							'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+					},
+					autosize: true,
+					showlegend: false,
+					paper_bgcolor: 'transparent',
+					plot_bgcolor: 'transparent',
+					modebar: {
+						activecolor: '#082f49',
+						color: '#082f49',
+						bgcolor: 'transparent',
+						orientation: 'v',
+						remove: 'lasso2d'
+					}
+				}}
+				debounce={250}
+			/>
+		</div>
+
+		<div class="flex flex-row gap-2">
+			<input
+				type="text"
+				placeholder=" demos.tf id"
+				class="rounded-md transition hover:shadow-md hover:ring hover:ring-sky-200"
+			/>
+			<div
+				class="
+				flex
+				items-center
+				justify-center
+				shadow-md
+				rounded-md
+				box-border
+				h-10
+				w-32
+				bg-gradient-to-r
+				from-indigo-500
+				from-10%
+				via-sky-500
+				via-30%
+				to-emerald-500
+				to-90%
+				transition
+				hover:ring
+				hover:ring-sky-200
+				"
+			>
+				<p class="font-bold text-sky-50">Analyze!</p>
+			</div>
+		</div>
+	</div>
+</div>
